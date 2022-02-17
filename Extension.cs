d71e656b813a54d8b899afd528ac6dc3d04bbcfc
isjcode <98400721+isjcode@users.noreply.github.com>
2022-02-17 07:52:18 +0100

    Add files via upload

---
+++ Extension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ConsoleApp1
+{
+    static class Etxension
+    {
+        public static bool IsEven(this int i)
+        {
+            return i % 2 == 0;
+        }
+
+        public static int[] GetValueIndexes(this int[] int_array, int i)
+        {
+            int[] ret = { };
+            for (int a = 0; a < int_array.Length; a++)
+            {
+                if (int_array[a] == i)
+                {
+                   Array.Resize(ref ret, ret.Length + 1);
+                   ret[ret.Length - 1] = a;
+                }
+            }
+
+            return ret;
+        }
+    }
+}
